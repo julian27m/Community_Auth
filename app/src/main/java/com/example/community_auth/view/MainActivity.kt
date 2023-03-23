4cdc068db0268dc63cdd27a01d8bc99e158f7a97
Julián Mora <j.morav@uniandes.edu.co>
2023-03-23 12:02:55 -0500

    Navigation Components in the app

--- app/src/main/java/com/example/community_auth/view/MainActivity.kt
+++ app/src/main/java/com/example/community_auth/view/MainActivity.kt
@@ -1,40 +1,140 @@
 package com.example.community_auth.view
 
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
+import android.view.*
+import android.widget.*
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.community_auth.R
 import com.example.community_auth.model.LoginActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationBarView
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity: AppCompatActivity() {
-    lateinit var userName: TextView
-    lateinit var logout: Button
-    lateinit var gClient: GoogleSignInClient
-    lateinit var gOptions: GoogleSignInOptions
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var fab: FloatingActionButton
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        logout = findViewById(R.id.logout)
-        userName = findViewById(R.id.userName)
-        gOptions =
-            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
-        gClient = GoogleSignIn.getClient(this, gOptions!!)
-        val gAccount = GoogleSignIn.getLastSignedInAccount(this)
-        if (gAccount != null) {
-            val gName = gAccount.displayName
-            userName.setText(gName)
+
+        fab = findViewById(R.id.fab)
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open_nav,
+            R.string.close_nav
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+            navigationView.setCheckedItem(R.id.nav_home)
         }
-        logout.setOnClickListener(View.OnClickListener {
-            gClient!!.signOut().addOnCompleteListener {
-                finish()
-                startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+
+
+        replaceFragment(HomeFragment())
+        bottomNavigationView.setBackground(null)
+        bottomNavigationView.setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener { item: MenuItem ->
+            when (item.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.profile -> replaceFragment(ProfileFragment())
             }
+            true
         })
+        fab.setOnClickListener(View.OnClickListener { showBottomDialog() })
+
+
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment()).commit()
+            R.id.nav_help -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HelpFragment()).commit()
+            R.id.nav_about -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, AboutFragment()).commit()
+            R.id.nav_support -> supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SupportFragment()).commit()
+
+            //logout
+            R.id.nav_logout -> {
+                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestEmail()
+                    .build()
+                val mGoogleSignInClient: GoogleSignInClient = GoogleSignIn.getClient(this, gso)
+                mGoogleSignInClient.signOut()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+    }
+
+    private fun showBottomDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.bottomsheetlayout)
+        val donationLayout = dialog.findViewById<LinearLayout>(R.id.layoutDonation)
+        val requestLayout = dialog.findViewById<LinearLayout>(R.id.layoutRequest)
+        val cancelButton = dialog.findViewById<ImageView>(R.id.cancelButton)
+        donationLayout.setOnClickListener {
+            dialog.dismiss()
+            Toast.makeText(this@MainActivity, "Upload donation is clicked", Toast.LENGTH_SHORT)
+                .show()
+        }
+        requestLayout.setOnClickListener {
+            dialog.dismiss()
+            Toast.makeText(this@MainActivity, "I have a request is Clicked", Toast.LENGTH_SHORT)
+                .show()
+        }
+        cancelButton.setOnClickListener { dialog.dismiss() }
+        dialog.show()
+        dialog.window!!.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window!!.attributes.windowAnimations = R.style.DialogAnimation
+        dialog.window!!.setGravity(Gravity.BOTTOM)
     }
 }
